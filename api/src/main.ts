ebd97f4770a88e4bf24133dba20244c6247601e8
Manya Singh <singhmanya309@gmail.com>
2026-07-17 17:34:51 +0530

    Fix static file serving path for uploaded photos

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,15 @@ import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
+  prefix: '/uploads/',
+});
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
